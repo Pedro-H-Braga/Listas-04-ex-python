858fdcf3761912aac8e03432e7239b87d44e7d21
Pedro-H-Braga <pedrobragalima@gmail.com>
2022-12-15 20:33:06 -0300

    01

--- script01.py
+++ script01.py
@@ -5,19 +5,27 @@ resolução:
 - fazer a string ocorrer um 'split' que separe as palavras e seja possível contar as vogais
 - fazer laço que identifique as vogais nas strings 
 - fazer um contador para contar as aparições no laço
+
+ANDAR PELA STRING:
+var = '0123456789'
+x = var[0:3:2]
+RESPOSTA: X=012
+
+Fazer um laço que ande do inicio ao fim da
 '''
 # teste string
 entrada = 'opa digai meu bom, quão á de ser'
+letra = ''
 # contador de vogais
 contador = 0
 vogais = 'aeiouáéíóúãõâêîôû'
 # transformar as entradas em minusculo para não ter divergencia na contagem
 entrada = entrada.lower()
 
-if vogais in entrada:
-    contador += 1
-    print(vogais)
+#while letra in entrada:
+if letra in vogais:
+        contador += 1
 
-print(contador)
+print(f"A palavra '{entrada}' tem {contador} vogais.".format(entrada, contador))
 
 
