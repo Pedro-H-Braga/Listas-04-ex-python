35d2d291e90628dd154a87d66b38c3b66315014e
Pedro-H-Braga <pedrobragalima@gmail.com>
2022-12-15 20:06:26 -0300

    q 01

--- script01.py
+++ script01.py
@@ -1,1 +1,15 @@
+'''
+ASSUNTO DE STRINGS:
+
+resolução:
+- fazer a string ocorrer um 'split' que separe as palavras e seja possível contar as vogais
+- fazer laço que identifique as vogais nas strings 
+- fazer um contador para contar as aparições no laço
+
+'''
+# contador para as vogais
+entrada = 'opa digai meu bom, quão á de ser'
+
+
+
 
