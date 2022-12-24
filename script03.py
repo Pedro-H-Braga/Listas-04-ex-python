6763756616d1fc9b1084f7608b1ce9bd40f1fdda
Pedro-H-Braga <pedrobragalima@gmail.com>
2022-12-24 15:40:10 -0300

    script03

---
+++ script03.py
@@ -0,0 +1,11 @@
+'''
+Faça um programa que solicite ao usuário uma frase, uma palavra antiga e uma palavra nova. O
+programa deverá imprimir a frase digitada, as duas palavras digitadas (a antiga e a nova) e a frase
+substituindo a palavra antiga pela palavra nova.
+'''
+#duas entrada 
+entrada_frase = input("informe uma frase: ")
+entrada_antiga = input("informe a palavra antiga: ")
+entrada_nova = input("informe a palavra nova: ")
+
+# achar a palavra_antiga na frase e dar um replace, exibir a palavra antiga e a nova 
