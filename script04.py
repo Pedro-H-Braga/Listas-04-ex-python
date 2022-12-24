b63042c1fdcb9575a12a30d6d12f34c6bddacb45
Pedro-H-Braga <pedrobragalima@gmail.com>
2022-12-24 15:55:53 -0300

    script04

---
+++ script04.py
@@ -0,0 +1,9 @@
+'''
+Faça um programa que receba um valor qualquer e informe se esse valor digitado é um palíndromo
+(utilizar laço de repetição WHILE).
+Lembrando que palíndromo é uma cadeia de caracteres que quando lida da esquerda para a direita é
+igual a quando se lê da direita para esquerda.
+Exemplos: ARARA, 15051, ...
+
+pegar a string que verifica, quando a string(pos(1,2,3,4)) for igua a ela quando invertida (pos(-1,-2,-3,-4))
+'''
