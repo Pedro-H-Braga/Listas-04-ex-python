9df9564634f2f23b5d61643f90db9ccddc87fbb8
Pedro-H-Braga <pedrobragalima@gmail.com>
2022-12-24 17:38:08 -0300

    script08

--- script08.py
+++ script08.py
@@ -2,5 +2,9 @@
 Faça um programa que leia um CPF (considerar somente que sejam informados os 11 dígitos numéricos)
 e verificar se o CPF é válido (pesquisar fórmula de cálculo do dígito verificador do CPF).
 
+entrada inteira
+tamanho_entrada = len(entrada)
 
+if tamanho_entrada == 11:
+     
 '''
