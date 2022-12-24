388d024b49cadeab8619c757707c1507189f0023
Pedro-H-Braga <pedrobragalima@gmail.com>
2022-12-24 14:24:29 -0300

    script 01

--- script01.py
+++ script01.py
@@ -28,19 +28,20 @@ percorre_vogais = ''
 tamanho_vogais = len(vogais)                    # pega o tamanho da string
 
 contador_string = 0                             # contador string
-entrada = 'opa digai meu bom, quão á de ser'    # tem 14 vogais
+entrada = input('digite algo: ')    # tem 14 vogais
 percorre_string = ''
 tamanho_string = len(entrada)                   # pega o tamanho da string           
                
 entrada = entrada.lower()                       # transformar as entradas em minusculo para não ter divergencia na contagem
 
-while contador_string < tamanho_string: 
+while contador_string < tamanho_string:         # percorre a string até o contador chegar ao tamanho máximo dela
     percorre_string = entrada[contador_string::tamanho_string]
     contador_string += 1
     percorre_vogais = vogais[contador_vogais::tamanho_vogais]
     contador_vogais += 1
     if percorre_vogais in percorre_string:
-                contador_vogais += 1
+        contador_vogais += 1
     elif percorre_vogais not in percorre_string:
-                contador_string += 1
+        contador_string += 1
+
 print(f"Tem {contador_vogais} vogais na palavra: {entrada}")
