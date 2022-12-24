352814e051ef7417a906ae9a6054803d3e5004e9
Pedro-H-Braga <pedrobragalima@gmail.com>
2022-12-24 15:31:19 -0300

    script 02

--- script02.py
+++ script02.py
@@ -2,12 +2,6 @@
 Faça um programa que solicite ao usuário uma frase e substitua todos os espaços por _ 
 (underline/sublinhado). O programa deverá exibir a frase original e a frase após as substituições.
 
-- pegar entrada
-- ver seu tamanho
-- variaveis: entrada, contador, percorre_string, tamanho_string
-
-varre a string. ao varrer:
-condição que se 'espaço em branco' em string substitua por '_'
 '''
 entrada = input("informe algo: ")
 
