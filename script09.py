d8d93af6783edee8d6ca3772ccc2f68734088d78
Pedro-H-Braga <pedrobragalima@gmail.com>
2022-12-25 22:13:54 -0300

    script 09

--- script09.py
+++ script09.py
@@ -25,15 +25,18 @@ acertos, erros, falta_quanto = 0,0,0
 tamanho_palavra = len(palavra_chave)
 
 while True:
+    print('!!!!!!!!!!BEM VINDO AO JOGO DA FORCA!!!!!!!!!!')
+    print('!!!!VOCE TEM 6 TENTATIVAS DE ERROS NO TOTAL!!!!')
 
     entrada = input('INFORME UMA LETRA: ').lower()    
     
     if entrada in palavra_chave:        # se tem a palavra na frase, acertos soma mais um 
         acertos += 1
-
+        print(f'voce tem "{acertos}" acertos e "{erros}" erros')
     
     elif entrada not in palavra_chave:  # se diferente erros soma mais um 
         erros += 1
+        print(f'voce tem "{acertos}" acertos e "{erros}" erros')
 
     # se quantidade de acertos igual a palavra chave, saia do programa e exiba voce ganhou
     if acertos == tamanho_palavra:
