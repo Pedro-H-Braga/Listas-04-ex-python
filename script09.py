dc3c9edadbbed7d047a8dc16aa3f0cf26c011e7f
Pedro-H-Braga <pedrobragalima@gmail.com>
2022-12-25 22:07:41 -0300

    script09

--- script09.py
+++ script09.py
@@ -13,12 +13,32 @@ erros
 falta_quanto
 
 LÓGICA:
-fazer laço que pegue entradas até completar a palavra e que se diferente da letra da palavra pegue o contador e some mais um
-até completar 6, se chegar em 6 saia do laço. 
-Quando pegar as entradas, varrer a string palavra_chave verificando se tem alguma letra informado que tem na palavra_chave,
-se sim, conte mais um para acertos e mostre quantas letras faltam para completar a palavra, 
-se não conte mais um para erros e mostre quantos erros já tem.
+fazer laço que pegue entradas até completar a palavra e que se diferente da letra da palavra pegue
+ o contador e some mais um até completar 6, se chegar em 6 saia do laço. 
+Quando pegar as entradas, varrer a string palavra_chave verificando se tem alguma letra informado
+que tem na palavra_chave, se sim, conte mais um para acertos e mostre quantas letras faltam para 
+completar a palavra, se não conte mais um para erros e mostre quantos erros já tem.
 Deixar todas as letras minuscula depois da entrada
-
-
 '''
+palavra_chave = 'jose'
+acertos, erros, falta_quanto = 0,0,0
+tamanho_palavra = len(palavra_chave)
+
+while True:
+
+    entrada = input('INFORME UMA LETRA: ').lower()    
+    
+    if entrada in palavra_chave:        # se tem a palavra na frase, acertos soma mais um 
+        acertos += 1
+
+    
+    elif entrada not in palavra_chave:  # se diferente erros soma mais um 
+        erros += 1
+
+    # se quantidade de acertos igual a palavra chave, saia do programa e exiba voce ganhou
+    if acertos == tamanho_palavra:
+        print(f'****PARABENS****, voce descobriu a palavra chave: {palavra_chave}')
+        break
+    elif erros > 5:
+        print(f'****QUE PENA VOCE PERDEU****, a palavra chave era: {palavra_chave}')
+        break
